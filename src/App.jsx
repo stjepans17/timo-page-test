2026fc5ae16e01042669d18bc8bd26d3a629816d
stjepans17 <ss85171@student.uni-lj.si>
2024-09-13 20:59:29 +0200

    aosos

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,12 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    // const [deviceType, setDeviceType] = useState('PC');
-
     useEffect(() => {
         const ua = navigator.userAgent || navigator.vendor;
         const isInstagram = ua.indexOf("Instagram") > -1 ? true : false;
         const userAgent = navigator.userAgent;
-        const link = "https://www.onlyfans.com/";
+        const link = "https://onlyfans.com/kathrynn";
         const safariLink = `x-web-search://?${link}`;
-
-        // if (isInstagram) {
-        //     if (/iPad|iPhone|iPod/.test(ua)) {
-        //         window.location.href = safariLink;
-        //         return;
-        //     // } else if (/android/i.test(userAgent)) {
-        //     //     window.location.href = "https://onlyfans.com";
-        //     //     return;
-        //     // } else {
-        //     //     window.location.href = link;
-        //     //     return;
-        //     // }
-        // } else {
-        //     window.location.href = link;
-        //     return;
-        // }
 
         if (/iPad|iPhone|iPod/.test(userAgent)) {
             window.location.href = safariLink;
