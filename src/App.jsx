1dd463f673fc7a68f0e678a02be5f45657ebbe07
stjepans17 <ss85171@student.uni-lj.si>
2024-09-15 17:33:15 +0200

    aosjdndfn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -15,7 +13,7 @@ function App() {
             window.location.href = safariLink;
             return;
         } else {
-            window.location.href = link; // Adjust the URL as needed
+            window.location.href = link;
             return;
         }
     }, []);
